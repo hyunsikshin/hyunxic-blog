d32569c912d17751cba4a594fb169c95a9734cb0
hyunsik <hyunsik.dev@gmail.com>
2019-02-12 12:24:02 +0900

    First Commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `HYUNSIK BLOG`,
+  description: `Blog posted about ...`,
+  author: `HYUNSIK`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: `http://hyunxic.me`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `hyunsikshin`, // Your GitHub account
+    medium: `@hyeon_ss`, // Your Medium account
+    facebook: `HyeonSS`, // Your Facebook account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'hyunsikshin/hyunxic-blog', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'hyunsik',
+  },
+  share: {
+    facebookAppId: '2684106998298387', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
